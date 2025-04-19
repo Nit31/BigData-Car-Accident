58e83d8474d74e395ebf31a0f8a660fc46e024b6
Nit31 <dydalinnnov31@gmail.com>
2025-04-19 18:13:40 +0200

    Added importing data to hdfs

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -9,3 +9,12 @@ pip install -r requirements.txt
 
 # Run the Python script to create and load the PostgreSQL database
 python3 -m scripts.build_projectdb
+
+# Clear the HDFS directory if it exists
+hdfs dfs -rm -r -f /user/team13/project/warehouse
+
+# Import the data from PostgreSQL to HDFS using Sqoop
+password=$(head -n 1 secrets/.psql.pass)
+sqoop import --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team13_projectdb \
+--username team13 --password $password --compression-codec=gzip --compress \
+--as-parquetfile --warehouse-dir=/user/team13/project/warehouse --m 1 --table accidents
