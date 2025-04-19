43e8745ca0b035f544f6cc4514a5d12fcd3802d0
Nit31 <dydalinnnov31@gmail.com>
2025-04-19 16:11:29 +0200

    Stage 1

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+sed -E -i '' \
+  -e '1s/Distance\(mi\)/Distance_mi/' \
+  -e '1s/Weather_Timestamp\?/Weather_Timestamp/' \
+  -e '1s/Temperature\(F\)/Temperature_F/' \
+  -e '1s/Wind_Chill\(F\)/Wind_Chill_F/' \
+  -e '1s/Humidity\(%\)/Humidity_percent/' \
+  -e '1s/Pressure\(in\)/Pressure_in/' \
+  -e '1s/Visibility\(mi\)/Visibility_mi/' \
+  -e '1s/Wind_Speed\(mph\)/Wind_Speed_mph/' \
+  -e '1s/Precipitation\(in\)/Precipitation_in/' \
+  data/data.csv
---
+++ sql/create_tables.sql
@@ -0,0 +1,34 @@
+START TRANSACTION;
+
+DROP TABLE accidents CASCADE;
+
+-- Create the accidents table
+CREATE TABLE IF NOT EXISTS accidents (
+    ID VARCHAR(9) NOT NULL PRIMARY KEY,
+    Severity INT NOT NULL,
+    Start_Time TIMESTAMP NOT NULL,
+    End_Time TIMESTAMP NOT NULL,
+    Start_Lat DECIMAL(10, 7) NOT NULL,
+    Start_Lng DECIMAL(10, 7) NOT NULL,
+    End_Lat DECIMAL(10, 7),
+    End_Lng DECIMAL(10, 7),
+    Distance_mi DECIMAL(10, 2) NOT NULL,
+    Side CHAR(1),
+    City VARCHAR(255),
+    County VARCHAR(255),
+    State CHAR(2),
+    Weather_Timestamp TIMESTAMP,
+    Temperature_F DECIMAL(5, 2),
+    Wind_Chill_F DECIMAL(5, 2),
+    Humidity_percent DECIMAL(5, 2),
+    Pressure_in DECIMAL(5, 2),
+    Visibility_mi DECIMAL(5, 2),
+    Wind_Speed_mph DECIMAL(5, 2),
+    Precipitation_in DECIMAL(5, 5),
+    Weather_Condition VARCHAR(255),
+    Sunrise_Sunset VARCHAR(50)
+);
+
+ALTER DATABASE team0_projectdb SET datestyle TO iso, ymd;
+
+COMMIT;
